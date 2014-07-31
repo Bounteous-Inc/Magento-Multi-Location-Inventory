dcbc115c972aebbcfdbc3c9e5174cf65079265dd
Matt Dunbar <matt@buildrx.com>
2014-07-30 20:20:43 -0400

    Removing unnecessary sql installer.

--- app/code/community/Demac/MultiLocationInventory/sql/demac_multilocationinventory_setup/upgrade-1.1.6-1.1.7.php
+++
@@ -1,9 +0,0 @@
-<?php
-$installer = $this;
-
-$installer->startSetup();
-
-//You probably shouldn't enable / deploy this during the day if you have a big catalog and/or a lot of locations...
-//Mage::getModel('demac_multilocationinventory/indexer')->reindexAll();
-
-$installer->endSetup();
