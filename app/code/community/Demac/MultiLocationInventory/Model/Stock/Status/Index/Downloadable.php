da7bc6404d52b1dcf4702139e56d2844c2025a91
Matt Dunbar <matt@buildrx.com>
2014-08-20 09:50:05 -0400

    Added downloadable product indexer

---
+++ app/code/community/Demac/MultiLocationInventory/Model/Stock/Status/Index/Downloadable.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Class Demac_MultiLocationInventory_Model_Stock_Status_Index_Simple
+ */
+class Demac_MultiLocationInventory_Model_Stock_Status_Index_Downloadable
+    extends Demac_MultiLocationInventory_Model_Stock_Status_Index_Simple
+    implements Demac_MultiLocationInventory_Model_Stock_Status_Index_Interface
+{
+    protected $productType = 'downloadable';
+}
