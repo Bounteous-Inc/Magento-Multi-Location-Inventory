07245ca03578c35e37968b7df8376485bc1e93d2
Matt Dunbar <matt@buildrx.com>
2014-08-25 10:35:29 -0400

    bug fix; implements instead of extends...

--- app/code/community/Demac/MultiLocationInventory/Model/Stock/Status/Index/Configurable.php
+++ app/code/community/Demac/MultiLocationInventory/Model/Stock/Status/Index/Configurable.php
@@ -4,7 +4,7 @@
  * Class Demac_MultiLocationInventory_Model_Stock_Status_Index_Configurable
  */
 class Demac_MultiLocationInventory_Model_Stock_Status_Index_Configurable
-    implements Demac_MultiLocationInventory_Model_Stock_Status_Index_Grouped
+    extends Demac_MultiLocationInventory_Model_Stock_Status_Index_Grouped
 {
     protected $productType = 'configurable';
 }
