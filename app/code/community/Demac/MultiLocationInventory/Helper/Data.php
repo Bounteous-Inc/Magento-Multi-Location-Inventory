2db98b3495720b8bbea2001b43423f40987f8b3c
Matt Dunbar <matt@mattdsworld.com>
2014-08-28 15:23:38 -0400

    removed curl code that was accidentally left

--- app/code/community/Demac/MultiLocationInventory/Helper/Data.php
+++ app/code/community/Demac/MultiLocationInventory/Helper/Data.php
@@ -73,7 +73,6 @@ class Demac_MultiLocationInventory_Helper_Data extends Mage_Core_Helper_Abstract
             $ch         = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
             $response = curl_exec($ch);
