10653a045974726ff76731f62329df0c0bca1c41
Matt Dunbar <matt@mattdsworld.com>
2014-09-15 15:57:35 -0400

    Update upgrade-1.2.3-1.2.4.php

--- app/code/community/Demac/MultiLocationInventory/sql/demac_multilocationinventory_setup/upgrade-1.2.3-1.2.4.php
+++ app/code/community/Demac/MultiLocationInventory/sql/demac_multilocationinventory_setup/upgrade-1.2.3-1.2.4.php
@@ -523,6 +523,182 @@ SET    dest.qty = src.qty,
 WHERE  dest.store_id = src.store_id
        AND dest.product_id = src.product_id;
 END;
+
+-- check if all fields are optional
+CREATE temporary TABLE IF NOT EXISTS
+demac_multilocationinventory_bundled_indexer_tmp AS
+  (SELECT catalog_product_entity.entity_id,
+          Sum(catalog_product_bundle_option.required) AS required_count
+   FROM   catalog_product_entity
+          JOIN catalog_product_bundle_option
+            ON catalog_product_bundle_option.parent_id =
+               catalog_product_entity.entity_id
+   WHERE  type_id = 'bundle'
+   GROUP  BY catalog_product_entity.entity_id);
+
+-- ALL OPTIONAL: get all child products, if sum of is_in_stock > 0 then set is_in_stock = 1 and qty = some really big number
+-- GLOBAL:
+UPDATE demac_multilocationinventory_stock_status_index dest,
+       (SELECT catalog_product_bundle_selection.parent_product_id AS product_id,
+               IF(Sum(is_in_stock) > 0, 99999, 0)                 AS qty,
+               IF(Sum(is_in_stock) > 0, 1, 0)                     AS is_in_stock
+        FROM   catalog_product_bundle_selection
+               JOIN demac_multilocationinventory_bundled_indexer_tmp
+                 ON demac_multilocationinventory_bundled_indexer_tmp.entity_id =
+                    catalog_product_bundle_selection.parent_product_id
+               JOIN demac_multilocationinventory_stock
+                 ON demac_multilocationinventory_stock.product_id =
+                    catalog_product_bundle_selection.product_id
+        WHERE  required_count = 0
+        GROUP  BY catalog_product_bundle_selection.parent_product_id) src
+SET    dest.qty = src.qty,
+       dest.is_in_stock = src.is_in_stock,
+       dest.backorders = 0,
+       dest.manage_stock = 1
+WHERE  dest.store_id = 0
+       AND dest.product_id = src.product_id;
+
+-- STORE SPECIFIC:
+UPDATE demac_multilocationinventory_stock_status_index dest,
+       (SELECT demac_multilocationinventory_stores.store_id
+               AS
+               store_id,
+               catalog_product_bundle_selection.parent_product_id
+               AS
+               product_id,
+               IF(Sum(demac_multilocationinventory_stock.is_in_stock) > 0, 99999
+               , 0) AS
+               qty,
+               IF(Sum(demac_multilocationinventory_stock.is_in_stock) > 0, 1, 0)
+               AS
+               is_in_stock
+        FROM   catalog_product_bundle_selection
+               JOIN demac_multilocationinventory_bundled_indexer_tmp
+                 ON demac_multilocationinventory_bundled_indexer_tmp.entity_id =
+                    catalog_product_bundle_selection.parent_product_id
+               JOIN demac_multilocationinventory_stock
+                 ON demac_multilocationinventory_stock.product_id =
+                    catalog_product_bundle_selection.product_id
+               JOIN demac_multilocationinventory_stores
+                 ON demac_multilocationinventory_stock.location_id =
+                    demac_multilocationinventory_stores.location_id
+        WHERE  required_count = 0
+        GROUP  BY Concat(catalog_product_bundle_selection.parent_product_id, '_'
+       ,
+                             demac_multilocationinventory_stores.store_id)) src
+SET    dest.qty = src.qty,
+       dest.is_in_stock = src.is_in_stock,
+       dest.backorders = 0,
+       dest.manage_stock = 1
+WHERE  dest.store_id = src.store_id
+       AND dest.product_id = src.product_id;
+
+-- SOME REQUIRED: Verify that each required option has at least one child item in stock (sum is_in_stock grouped by option, if all are greater than 1)
+-- GLOBAL:
+UPDATE demac_multilocationinventory_stock_status_index dest,
+       (SELECT src.product_id
+               AS
+               product_id,
+               IF(Concat(',', Group_concat(src.is_in_stock), ',') LIKE ',0,', 0,
+               1)
+               AS
+               is_in_stock,
+               IF(Concat(',', Group_concat(src.is_in_stock), ',') LIKE ',0,', 0,
+               99999)
+               AS qty
+        FROM   (SELECT catalog_product_bundle_selection.option_id
+                       AS
+                               option_id,
+                       catalog_product_bundle_selection.parent_product_id
+                       AS
+                               product_id,
+                       IF(Sum(demac_multilocationinventory_stock.is_in_stock) >
+                          0, 1, 0
+                       ) AS
+                               is_in_stock
+                FROM   catalog_product_bundle_selection
+                       JOIN demac_multilocationinventory_bundled_indexer_tmp
+                         ON
+demac_multilocationinventory_bundled_indexer_tmp.entity_id
+=
+catalog_product_bundle_selection.parent_product_id
+                       JOIN catalog_product_bundle_option
+                         ON catalog_product_bundle_option.option_id =
+                            catalog_product_bundle_selection.option_id
+                       JOIN demac_multilocationinventory_stock
+                         ON demac_multilocationinventory_stock.product_id =
+                            catalog_product_bundle_selection.product_id
+                WHERE  required_count > 0
+                       AND catalog_product_bundle_option.required = 1
+                GROUP  BY Concat(
+                catalog_product_bundle_selection.parent_product_id,
+                          '_',
+                                     catalog_product_bundle_selection.option_id)
+               ) src
+        GROUP  BY Concat(src.product_id)) src2
+SET    dest.qty = src2.qty,
+       dest.is_in_stock = src2.is_in_stock,
+       dest.backorders = 0,
+       dest.manage_stock = 1
+WHERE  dest.store_id = 0
+       AND dest.product_id = src2.product_id;
+
+-- STORE SPECIFIC:
+UPDATE demac_multilocationinventory_stock_status_index dest,
+       (SELECT src.store_id,
+               src.product_id,
+               IF(Concat(',', Group_concat(src.is_in_stock), ',') LIKE ',0,', 0,
+               1)
+               AS
+               is_in_stock,
+               IF(Concat(',', Group_concat(src.is_in_stock), ',') LIKE ',0,', 0,
+               99999)
+               AS qty
+        FROM   (SELECT demac_multilocationinventory_stores.store_id
+                       AS
+                               store_id,
+                       catalog_product_bundle_selection.option_id
+                       AS
+                               option_id,
+                       catalog_product_bundle_selection.parent_product_id
+                       AS
+                               product_id,
+                       IF(Sum(demac_multilocationinventory_stock.is_in_stock) >
+                          0, 1, 0
+                       ) AS
+                               is_in_stock
+                FROM   catalog_product_bundle_selection
+                       JOIN demac_multilocationinventory_bundled_indexer_tmp
+                         ON
+demac_multilocationinventory_bundled_indexer_tmp.entity_id
+=
+catalog_product_bundle_selection.parent_product_id
+                       JOIN catalog_product_bundle_option
+                         ON catalog_product_bundle_option.option_id =
+                            catalog_product_bundle_selection.option_id
+                       JOIN demac_multilocationinventory_stock
+                         ON demac_multilocationinventory_stock.product_id =
+                            catalog_product_bundle_selection.product_id
+                       JOIN demac_multilocationinventory_stores
+                         ON demac_multilocationinventory_stock.location_id =
+                            demac_multilocationinventory_stores.location_id
+                WHERE  required_count > 0
+                       AND catalog_product_bundle_option.required = 1
+                GROUP  BY Concat(
+                catalog_product_bundle_selection.parent_product_id,
+                          '_',
+demac_multilocationinventory_stores.store_id, '_',
+catalog_product_bundle_selection.option_id)) src
+GROUP  BY Concat(src.product_id, '_', src.store_id)) src2
+SET    dest.qty = src2.qty,
+       dest.is_in_stock = src2.is_in_stock,
+       dest.backorders = 0,
+       dest.manage_stock = 1
+WHERE  dest.store_id = src2.store_id
+       AND dest.product_id = src2.product_id;
+
+-- CLEANUP:
+DROP TABLE IF EXISTS demac_multilocationinventory_bundled_indexer_tmp;
 ';
 
 $write = Mage::getSingleton('core/resource')->getConnection('core_write');
